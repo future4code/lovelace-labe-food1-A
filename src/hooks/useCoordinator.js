ebb35c36fa9701b8012a51179252b812b3b46042
cleitonsoares-s <cleiton.ugo@hotmail.com>
2021-09-06 17:40:36 -0300

    FEAT: header finalizado

---
+++ src/hooks/useCoordinator.js
@@ -0,0 +1,18 @@
+import { useHistory } from 'react-router-dom'
+
+export const useCoordinator = () => {
+  let history = useHistory()
+  const goTo = {
+    Cart: () => history.push('/cart'),
+    EditAdress: () => history.push('/profile/edit-address'),
+    EditProfile: () => history.push('/profile/edit-profile'),
+    Home: () => history.push('/'),
+    Login: () => history.push('/login'),
+    Profile: () => history.push('/profile'),
+    RestaurantDetail: (id) => history.push(`/restaurants/${id}`),
+    SignUp: () => history.push('/signup'),
+    SignUpAdress: () => history.push('/signup-address'),
+  }
+
+  return { goTo }
+}
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Header from '../../components/Header'
 
 const Home = () => {
     return (
         <div>
-            
+          <Header backButton={true} title={'Rappi4'}/>
         </div>
     )
 }
